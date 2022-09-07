2265a715f22131c50e0db26a95fe1a55ba86e774
Majid <m.yaghouti@yahoo.com>
2022-09-08 01:41:10 +0430

    fix getBy* methods

--- lib/index.ts
+++ lib/index.ts
@@ -1,49 +1,68 @@
 import * as fs from 'fs';
-import { ICountry, ICountryCurrency, ICountryCurrencyList, IDBItem } from "./interfaces";
+import {
+  ICountry,
+  ICountryCurrency,
+  ICountryCurrencyList,
+  IDBItem,
+} from './interfaces';
 
 export class CountryCurrencyIso {
   private readonly countries: Map<string, string>;
-  private readonly countryISOs: Map<string, string>;
+  private readonly countryISOs: Map<string, Set<string>>;
   private readonly currencies: Map<string, string>;
-  private readonly currencySymbols: Map<string, string>;
+  private readonly symbols: Map<string, string>;
   private readonly country_currency: Map<string, string>;
   private readonly currency_countries: Map<string, Set<string>>;
 
   constructor() {
+    this.countries = new Map<string, string>();
+    this.countryISOs = new Map<string, Set<string>>();
+    this.currencies = new Map<string, string>();
+    this.symbols = new Map<string, string>();
+    this.country_currency = new Map<string, string>();
+    this.currency_countries = new Map<string, Set<string>>();
     this.loadDb();
   }
 
-  public getByCountry(country: string): ICountryCurrency {
+  public getByCountry(country: string): ICountryCurrency | undefined {
     const iso = this.countries.get(country);
     if (!iso) {
-      return null;
+      return;
     }
-    const currency = this.country_currency.get(country);
-    if (!currency) {
-      return null;
-    }
-    const symbol = this.currencies.get(currency);
+    const currency = this.country_currency.get(country) ?? '';
+    const symbol = this.currencies.get(currency) ?? '';
     return { country, iso, currency, symbol };
   }
 
-  public getByISO(iso: string): ICountryCurrency {
-    const country = this.countryISOs.get(iso);
-    if (!country) {
-      return null;
+  public getByISO(iso: string): ICountryCurrencyList | undefined {
+    iso = iso.toUpperCase();
+    const countryNames = this.countryISOs.get(iso);
+    if (!countryNames || !countryNames.size) {
+      return;
     }
-    const currency = this.country_currency.get(country);
-    const symbol = this.currencies.get(currency);
-    return { country, iso, currency, symbol };
+    const countries: ICountry[] = [];
+    countryNames.forEach((country) => {
+      const countryCurrency = this.getByCountry(country);
+      if (countryCurrency) {
+        countries.push({ name: country, iso: countryCurrency.iso });
+      }
+    });
+    const item = this.getByCountry(countries[0].name);
+    return {
+      countries,
+      currency: item?.currency ?? '',
+      symbol: item?.symbol ?? '',
+    };
   }
 
-  public getByCurrency(currency: string): ICountryCurrencyList {
+  public getByCurrency(currency: string): ICountryCurrencyList | undefined {
     const symbol = this.currencies.get(currency);
     if (!symbol) {
-      return null;
+      return;
     }
-    const countryNames = this.currency_countries.get(currency);
+    const countryNames = this.currency_countries.get(currency) ?? [];
     const countries: ICountry[] = [];
-    countryNames.forEach(country => {
+    countryNames.forEach((country) => {
       const countryCurrency = this.getByCountry(country);
       if (countryCurrency) {
         countries.push({ name: country, iso: countryCurrency.iso });
@@ -52,14 +71,14 @@ export class CountryCurrencyIso {
     return { countries, currency, symbol };
   }
 
-  public getBySymbol(symbol: string): ICountryCurrencyList {
-    const currency = this.currencySymbols.get(symbol);
+  public getBySymbol(symbol: string): ICountryCurrencyList | undefined {
+    const currency = this.symbols.get(symbol);
     if (!currency) {
-      return null;
+      return;
     }
-    const countryNames = this.currency_countries.get(currency);
+    const countryNames = this.currency_countries.get(currency) ?? [];
     const countries: ICountry[] = [];
-    countryNames.forEach(country => {
+    countryNames.forEach((country) => {
       const countryCurrency = this.getByCountry(country);
       if (countryCurrency) {
         countries.push({ name: country, iso: countryCurrency.iso });
@@ -70,43 +89,60 @@ export class CountryCurrencyIso {
 
   public addToDB(item: IDBItem): void {
     this.countries.set(item.country, item.iso);
-    this.countryISOs.set(item.iso, item.country);
-    this.currencies.set(item.currency, item.symbol);
-    this.currencySymbols.set(item.symbol, item.currency);
-    this.country_currency.set(item.country, item.currency);
-    const countries = this.currency_countries.get(item.currency);
-    if (countries) {
-      countries.add(item.country);
+    let isoCountries = this.countryISOs.get(item.iso);
+    if (isoCountries) {
+      isoCountries.add(item.country);
     } else {
-      const countries = new Set<string>().add(item.country);
-      this.currency_countries.set(item.currency, countries);
+      isoCountries = new Set<string>().add(item.country);
+      this.countryISOs.set(item.iso, isoCountries);
+    }
+    this.currencies.set(item.currency, item.symbol);
+    this.symbols.set(item.symbol, item.currency);
+    this.country_currency.set(item.country, item.currency);
+    let currencyCountries = this.currency_countries.get(item.currency);
+    if (currencyCountries) {
+      currencyCountries.add(item.country);
+    } else {
+      currencyCountries = new Set<string>().add(item.country);
+      this.currency_countries.set(item.currency, currencyCountries);
     }
   }
 
   private loadDb(): void {
-    const countries = JSON.parse(fs.readFileSync('../db/countries.json', 'utf-8'));
-    const currencies = JSON.parse(fs.readFileSync('../db/currencies.json', 'utf-8'));
-    const country_currency = JSON.parse(fs.readFileSync('../db/country_currency.json', 'utf-8'));
+    const countries = JSON.parse(
+      fs.readFileSync('./db/countries.db.json', 'utf-8'),
+    );
+    const currencies = JSON.parse(
+      fs.readFileSync('./db/currencies.db.json', 'utf-8'),
+    );
+    const country_currency = JSON.parse(
+      fs.readFileSync('./db/country_currency.db.json', 'utf-8'),
+    );
 
     for (const country in countries) {
       this.countries.set(country, countries[country]);
-      this.countryISOs.set(countries[country], country);
+      let isoCountries = this.countryISOs.get(countries[country]);
+      if (isoCountries) {
+        isoCountries.add(country);
+      } else {
+        isoCountries = new Set<string>().add(country);
+        this.countryISOs.set(countries[country], isoCountries);
+      }
     }
     for (const currency in currencies) {
       this.currencies.set(currency, currencies[currency]);
-      this.currencySymbols.set(currencies[currency], currency);
+      this.symbols.set(currencies[currency], currency);
     }
     for (const cc in country_currency) {
       this.country_currency.set(cc, country_currency[cc]);
 
-      const countries = this.currency_countries.get(country_currency[cc]);
-      if (countries) {
-        countries.add(cc);
+      let currencyCountries = this.currency_countries.get(country_currency[cc]);
+      if (currencyCountries) {
+        currencyCountries.add(cc);
       } else {
-        const countries = new Set<string>().add(cc);
-        this.currency_countries.set(country_currency[cc], countries);
+        currencyCountries = new Set<string>().add(cc);
+        this.currency_countries.set(country_currency[cc], currencyCountries);
       }
     }
   }
 }
-
